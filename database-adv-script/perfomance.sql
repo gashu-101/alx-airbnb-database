3c2bba414c9605e7b37ed7a30a56761c6797c351
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-12-01 21:43:54 +0300

    performance script

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -10,6 +10,28 @@ SELECT
     b.created_at AS booking_date
 FROM 
     bookings AS b
+WHERE 
+    b.id IN (
+        SELECT 
+            b.id
+        FROM 
+            bookings AS b
+        JOIN 
+            users AS u 
+        ON 
+            b.user_id = u.id
+        JOIN 
+            properties AS p 
+        ON 
+            b.property_id = p.id
+        JOIN 
+            payments AS pay 
+        ON 
+            b.payment_id = pay.id
+    )
+    
+AND
+    b.user_id = u.id
 JOIN 
     users AS u 
 ON 
